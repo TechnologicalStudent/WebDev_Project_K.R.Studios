343e6ed5f904ab96773cde9e71afbee2ebc00960
TechnologicalStudent <A00037216@myTUDublin.ie>
2025-12-01 14:54:31 +0000

    Add files via upload

--- JS/websiteJs.js
+++ JS/websiteJs.js
@@ -8,6 +8,29 @@ function moveChildOfIndex(){
 
 }
 
+function validateForm() {
+// retrieving form values
+let name = document.getElementById("FirstName").value;
+let email = document.getElementById("email").value;
+let lastName = document.getElementById("LastName").value;
+
+// avoiding blank input
+if (name == "") {
+	alert("First Name is required");
+	
+}
+
+if (lastName == "") {
+	alert("Last Name is required");
+}
+// validating email format using a simple regular expression
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (!emailRegex.test(email)) {
+	alert("Please enter a valid email address");
+}
+
+}
+
 
  function detectMob() {
     return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
@@ -51,7 +74,12 @@ document.addEventListener('DOMContentLoaded', function() {
    let name = document.getElementById("name")
     let fullParagraph = document.getElementById("FullParagraph")
 
-     name.textContent = " "
+    if (name){
+        
+        name.textContent = " "
         fullParagraph.textContent = ""
+
+    }
+     
    
 }, false);
